57605af824903f8e98d207594fae11a9a39471ac
jb3dahmen <jb3dahmen@gmail.com>
2017-08-12 12:47:22 -0700

    Added more comments
    Still need some cleaning up

--- Player.py
+++ Player.py
@@ -1,10 +1,15 @@
 from Item import *
 
+'''This class defines how out player will behave. It "inherits" from its parent class Item so it can do 
+everything Item can do but we can also add more behavior without having to duplicate code. Pretty neat!'''
 class Player(Item):
     
+    #Here we set up the player
     def __init__(self, animage, xlocation, ylocation):
+        #We call on Player's parent constuctor to set up the basic image, location, and size information
         Item.__init__(self, animage, xlocation, ylocation, animage.width, animage.height)
         
+    #This is where we tell the player how to move left using its location
     def moveLeft(self):
         #print("Move Left");
         if(self.xlocation - 10 > 0):
@@ -16,6 +21,7 @@ class Player(Item):
             #PUT SOME CODE HERE
             self.xlocation -= 10
     
+    #This is where we tell the player how to move right using its location
     def moveRight(self):
         #print("Move Right");
         if(self.xlocation + 10 < width):
